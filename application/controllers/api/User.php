cec3974557a80ccbc615c73586bf6ebcfdf5fb27
Prince <princelonappan07@gmail.com>
2018-07-25 23:22:53 +0530

    Fixed logout issue

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -318,7 +318,7 @@ class User extends REST_Controller
         {
             if (isset($this->rest->user_id) && !empty($this->rest->user_id))
             {
-                $update_data['user_id'] = '';
+                $update_data['user_id'] = 0;
                 $this->Key_model->update_key_details($key, $update_data);
             }
             $this->response(array('result_code' => 200, 'result_title' => 'Success', 'result_string' => 'Successfuly logged out from the application.'));
